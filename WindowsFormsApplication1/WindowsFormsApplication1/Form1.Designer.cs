313fd9477c38a30bb39054098acc8c64baadddcc
Forestf <michalsliwa96@wp.pl>
2018-06-21 10:52:42 +0200

    NowePorzadki

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_prawo = new System.Windows.Forms.Button();
+            this.button_lewo = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(488, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove_1);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(12, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 300);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sprawdź";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(488, 337);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Wyczyść";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(713, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Skaluj";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(318, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(164, 329);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.textBox1.Location = new System.Drawing.Point(110, 378);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(622, 38);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button_prawo
+            // 
+            this.button_prawo.Location = new System.Drawing.Point(662, 306);
+            this.button_prawo.Name = "button_prawo";
+            this.button_prawo.Size = new System.Drawing.Size(20, 20);
+            this.button_prawo.TabIndex = 8;
+            this.button_prawo.Text = ">";
+            this.button_prawo.UseVisualStyleBackColor = true;
+            this.button_prawo.Click += new System.EventHandler(this.button_prawo_Click);
+            // 
+            // button_lewo
+            // 
+            this.button_lewo.Location = new System.Drawing.Point(600, 306);
+            this.button_lewo.Name = "button_lewo";
+            this.button_lewo.Size = new System.Drawing.Size(20, 20);
+            this.button_lewo.TabIndex = 9;
+            this.button_lewo.Text = "<";
+            this.button_lewo.UseVisualStyleBackColor = true;
+            this.button_lewo.Click += new System.EventHandler(this.button_lewo_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(626, 306);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(30, 30);
+            this.panel3.TabIndex = 10;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(800, 428);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button_lewo);
+            this.Controls.Add(this.button_prawo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_prawo;
+        private System.Windows.Forms.Button button_lewo;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,517 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bit;
+        Graphics g;
+        Graphics g1;
+        Graphics g2;
+        Graphics graph;
+        Rectangle rect;
+        Pen p;
+        Pen p1;
+        Pen p2;
+        Brush b;
+        Brush bc;
+        Brush bb;
+        int b_w=10, b_h=10;
+        int[][] rysowanie2;
+        int[][] wzor;
+        int[][] wzor_kolumn;
+        float[][] dziesiatka;
+        float[][] dziesiatka_k;
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            g1 = panel2.CreateGraphics();
+            g2 = panel3.CreateGraphics();
+            p = new Pen(Color.Red, 10);
+            p1 = new Pen(Color.White, 10);
+            p2 = new Pen(Color.Black, 1);
+            bc = new SolidBrush(Color.Red);
+            bb = new SolidBrush(Color.White);
+
+
+            rysowanie2 = new int[10][];
+            wzor = new int[10][];
+            wzor_kolumn = new int[10][];
+            dziesiatka = new float[10][];
+            dziesiatka_k = new float[10][];
+            for (int i = 0; i < 10; i++)
+            {
+                rysowanie2[i] = new int[10];
+                wzor[i] = new int[10];
+                wzor_kolumn[i] = new int[3];
+                dziesiatka[i] = new float[10];
+                dziesiatka_k[i] = new float[3];
+            }
+            //   b = new SolidBrush(Color.Red);
+            czytaj_tablice();
+            czytaj_tablice2();
+        }
+
+        //   private void panel1_MouseMove(object sender, MouseEventArgs e)
+        // {
+        //  if(e.Button)
+        //   g.DrawEllipse(p, e.X, e.Y, 10, 10);
+        //}
+
+
+        private void panel1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            int x = 0;
+            int w = 0, h = 0;
+            if (e.Button == MouseButtons.Left) { g.FillEllipse(bc, e.X, e.Y, b_w, b_h); x = 2; }
+            else if (e.Button == MouseButtons.Right) { g.FillEllipse(bb, e.X, e.Y, b_w, b_h); x = 1; }
+
+            if (x != 0)
+            {
+                for (int i = 1; i < 11; i++)
+                {
+                    if (e.Y < (panel2.Height / 10)) h = 1;
+                    else if (e.Y > (panel2.Height / 10) * (i - 1) && e.Y < (panel2.Height / 10) * i) h = i;
+                    if (e.X < (panel2.Width / 10)) w = 1;
+                    else if (e.X > (panel2.Width / 10) * (i - 1) && e.X < (panel2.Width / 10) * i) w = i;
+                }
+
+                
+                x = 0;
+            }
+
+        }
+
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            for (float i = panel2.Height / 10; i < panel2.Height; i += panel2.Height / 10)
+            {
+                g1.DrawLine(p2, 0, i, panel2.Width, i);
+
+            }
+            for (float i = panel2.Width / 10; i < panel2.Width; i += panel2.Width / 10)
+            {
+                g1.DrawLine(p2, i, 0, i, panel2.Height);
+
+            }
+
+
+
+
+        }
+
+
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            int x = 0;
+            if (e.Button == MouseButtons.Left) { b = new SolidBrush(Color.Red); x = 1; }
+            else if (e.Button == MouseButtons.Right) { b = new SolidBrush(Color.White); ; x = 0; }
+            if (b != null)
+            {
+                int w = 0, h = 0;
+
+                // for (int i = 1; i < 11; i++)
+                //{
+                //  if (e.X < (panel2.Width / 10)) w = 0;
+                //    else if (e.X > (panel2.Width / 10) * (i - 1) && e.X < (panel2.Width / 10) * i) w = i; 
+                //  }
+                for (int i = 1; i < 11; i++)
+                {
+                    if (e.Y < (panel2.Height / 10)) h = 1;
+                    else if (e.Y > (panel2.Height / 10) * (i - 1) && e.Y < (panel2.Height / 10) * i) h = i;
+                    if (e.X < (panel2.Width / 10)) w = 1;
+                    else if (e.X > (panel2.Width / 10) * (i - 1) && e.X < (panel2.Width / 10) * i) w = i;
+                }
+
+                float w1, w2, h1, h2;
+                w1 = (panel2.Width * (w - 1)) / 10;
+                w2 = panel2.Width / 10;
+                h1 = (panel2.Height * (h - 1)) / 10;
+                h2 = panel2.Height / 10;
+                if (e.Y % (panel2.Height / 10) != 0 && e.X % (panel2.Width / 10) != 0) rysowanie2[h - 1][w - 1] = x;
+                g1.FillRectangle(b, w1 + 1, h1 + 1, w2 - 1, h2 - 1);
+                b = null;
+            }
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != null) richTextBox1.Text="";
+            textBox1.Text = "";
+            bit = new Bitmap(panel2.Width, panel2.Height);
+            graph = Graphics.FromImage(bit);
+            rect = panel2.RectangleToScreen(panel2.ClientRectangle);
+            graph.CopyFromScreen(rect.Location, Point.Empty, panel2.Size);
+
+            
+            Color a = new Color();
+            
+
+            string komunikat = "Tablica jedynek:";
+
+            int H = 15;
+            
+            for (int i = 0; i < 10; i++)
+            {
+                int W = 15;
+                komunikat += System.Environment.NewLine;
+                for (int j = 0; j < 10; j++)
+                {
+                    a = bit.GetPixel(W, H);
+                    if (a == Color.FromArgb(255, 255, 0, 0)) rysowanie2[i][j] = 1;
+                        string temp;
+                    temp = Convert.ToString(rysowanie2[i][j]) + " ";
+                    komunikat += temp;
+                    W += 30;
+                }
+                H += 30;
+            }
+
+            richTextBox1.Text += System.Environment.NewLine + komunikat;
+            sprawdz();
+        }
+ 
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            bit = new Bitmap(panel1.Width, panel1.Height);
+            graph = Graphics.FromImage(bit);
+            rect = panel1.RectangleToScreen(panel1.ClientRectangle);
+            graph.CopyFromScreen(rect.Location, Point.Empty, panel1.Size);
+
+            int poczatek_X = 0, koniec_X = 0;
+            // Color a;
+            //a = new Color();
+            int biggest_Y = 0; int smallest_Y = panel1.Width;
+            for (int i = 0; i < 300; i += 1)
+            {
+
+                bool all_null_X = true;
+                for (int j = 0; j < 300; j += 1)
+                {
+
+
+                    Color a = new Color();
+                    a = bit.GetPixel(i, j);
+
+                    if (a == Color.FromArgb(255, 255, 0, 0))
+                    {
+                        if (poczatek_X == 0) { poczatek_X = i; all_null_X = false; }
+                        else if (poczatek_X != 0 && all_null_X) koniec_X = i - 1;
+
+                        if (j > biggest_Y) biggest_Y = j;
+                        if (j < smallest_Y) smallest_Y = j;
+                    }
+                   // if (all_null_X && poczatek_X == null) break;
+                }
+
+
+            }
+
+            graph.CopyFromScreen(rect.Location, Point.Empty, panel1.Size);
+
+
+            int Y = biggest_Y - smallest_Y;
+            int X = koniec_X - poczatek_X;
+            Size pitagoras = new Size();
+            // pitagoras = Math.Sqrt((Y * Y) + (X * X));
+            pitagoras.Height = Y;
+            pitagoras.Width = X;
+            int panel_X=panel1.Left;
+            int panel_Y = panel1.Top;
+
+            
+            Point punkt_p = new Point(poczatek_X, smallest_Y);
+
+            Point poczatkowy = new Point(rect.X+ poczatek_X, rect.Y+smallest_Y);
+            Size startowy_pitagoras = new Size();
+            startowy_pitagoras.Height = smallest_Y;
+            startowy_pitagoras.Width = poczatek_X;
+
+            Point konicowy = new Point();
+            konicowy = poczatkowy + pitagoras;
+
+            Bitmap zeskalowane = new Bitmap(X, Y);
+            Graphics graph2 = Graphics.FromImage(zeskalowane);
+            graph2.CopyFromScreen(rect.Location + startowy_pitagoras,Point.Empty, pitagoras);//zmienic ten add chyba
+
+
+            richTextBox1.Text = "Pierwotny rozmiar liczby:"+ System.Environment.NewLine+" W="+X+" H="+Y ;
+            int skala = 1;
+            for(int i=1; (zeskalowane.Height*skala)<300; i++)
+            {
+                skala = i;
+
+            }
+             Bitmap zeskalowane2 = new Bitmap(zeskalowane,zeskalowane.Width*skala, panel2.Height);
+
+            Graphics graph3 = Graphics.FromImage(zeskalowane);
+            graph3.InterpolationMode= System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+
+            panel2.BackgroundImageLayout = ImageLayout.Center;
+            panel2.BackgroundImage = zeskalowane2;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            panel2.Refresh();
+
+            for (float i = panel2.Height / 10; i < panel2.Height; i += panel2.Height / 10)
+            {
+                g1.DrawLine(p2, 0, i, panel2.Width, i);
+
+            }
+            for (float i = panel2.Width / 10; i < panel2.Width; i += panel2.Width / 10)
+            {
+                g1.DrawLine(p2, i, 0, i, panel2.Height);
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    rysowanie2[i][j] = 0;
+                }
+            }
+            panel1.BackgroundImage = null;
+            odsiwierz_p2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel2.BackgroundImage = null;
+            panel1.Refresh();
+            richTextBox1.Text = "";
+
+
+            panel2.Refresh();
+
+            for (float i = panel2.Height / 10; i < panel2.Height; i += panel2.Height / 10)
+            {
+                g1.DrawLine(p2, 0, i, panel2.Width, i);
+
+            }
+            for (float i = panel2.Width / 10; i < panel2.Width; i += panel2.Width / 10)
+            {
+                g1.DrawLine(p2, i, 0, i, panel2.Height);
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    rysowanie2[i][j] = 0;
+                }
+            }
+            
+          //  panel2.Refresh();
+
+        }
+
+
+        void odsiwierz_p2()
+        {
+            panel2.Refresh();
+        }
+
+        private void czytaj_tablice()
+        {
+            String input = File.ReadAllText(@"cyfry.txt");
+
+            int i = 0, j = 0;
+            
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    wzor[i][j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+
+        }
+
+
+        private void czytaj_tablice2()
+        {
+            String input = File.ReadAllText(@"cyfry2.txt");
+
+            int i = 0, j = 0;
+
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    wzor_kolumn[i][j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+
+        }
+
+        private void sprawdz()
+        {
+            
+
+            int[] narysowany = new int[10];
+
+            for(int i=0;i<10;i++)
+            {
+                int ile = 0;
+                for(int j=0; j<10;j++)
+                {
+                    if (rysowanie2[i][j] == 1) ile++;
+                }
+                narysowany[i] = ile;
+              
+            }
+
+
+            int[] narysowany2 = new int[3];
+
+            for (int i = 0; i <= 2; i++)
+            {
+                int ile = 0;
+                for (int j = 0; j < 10; j++)
+                {
+                    if (rysowanie2[j][i+3] == 1) ile++;
+                }
+                narysowany2[i] = ile;
+              
+
+            }
+
+            // int procenty = 100;  
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                   dziesiatka[i][j]= 10 - Math.Abs(narysowany[j] - wzor[i][j]);
+                    dziesiatka[i][j] = dziesiatka[i][j] *  0.7f ;
+                  //  richTextBox1.Text += + dziesiatka[i][j]+" ";
+                }
+               // richTextBox1.Text += System.Environment.NewLine;
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    dziesiatka_k[i][j] = 10 - Math.Abs(narysowany2[j] - wzor_kolumn[i][j]);
+                    //richTextBox1.Text += System.Environment.NewLine + dziesiatka_k[j][i];
+                    dziesiatka_k[i][j] = dziesiatka_k[i][j] ;
+                   //  richTextBox1.Text += dziesiatka_k[i][j]+" ";
+                }
+              // richTextBox1.Text += System.Environment.NewLine;
+            }
+
+
+            float[] ostatnia_pomocnicza_tabela = new float[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                float ile = 0.00f;
+                for (int j = 0; j < 3; j++)
+                {
+                    ile += dziesiatka_k[i][j];
+                }
+                ostatnia_pomocnicza_tabela[i] = ile;
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                float ile = 0.00f;
+                for (int j = 0; j < 10; j++)
+                {
+                    ile+=dziesiatka[i][j];
+                }
+                ostatnia_pomocnicza_tabela[i] += ile;
+
+            }
+            int odczytana = 0;
+            string komunikat="";
+            for (int i = 1; i < 10; i++)
+            {
+                if (ostatnia_pomocnicza_tabela[i] > ostatnia_pomocnicza_tabela[odczytana]) {odczytana = i; komunikat = ""; }
+                else if (ostatnia_pomocnicza_tabela[odczytana] == ostatnia_pomocnicza_tabela[i]) komunikat += " "+i + " albo ";
+            }
+            textBox1.Text = "Narysowana cyfra to"+komunikat + " "+odczytana;
+            komunikat = "";
+            for (int i=0; i<10;i++)
+            {
+
+                richTextBox1.Text += System.Environment.NewLine +i+"= "+ostatnia_pomocnicza_tabela[i]+"%";
+            }
+
+            for(int i=0; i<10;i++)
+            {
+                ostatnia_pomocnicza_tabela[i] = 0;
+                narysowany[i] = 0;
+                for(int j=0;j<10;j++)
+                {
+                    dziesiatka[i][j] = 0;
+                    rysowanie2[i][j] = 0;
+
+                }
+            }
+             
+
+        }
+
+        private void button_lewo_Click(object sender, EventArgs e)
+        {
+            if( b_w>5 &&b_h>5)
+            {
+                b_w--;
+                b_h--;
+                panel3.Refresh();
+            }
+        }
+
+        private void button_prawo_Click(object sender, EventArgs e)
+        {
+            if (b_w < 20 && b_h < 20)
+            {
+                b_w++;
+                b_h++;
+                panel3.Refresh();
+            }
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            g2.FillEllipse(bc,panel3.Height /2 -b_h/2, panel3.Width / 2 -b_w/2, b_w, b_h);
+        }
+    }
+
+}
